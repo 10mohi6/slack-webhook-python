df77ec91a73a02b3b97ca17b00dfd1823796259a
10mohi6 <10.mohi.6.y@gmail.com>
2019-09-20 17:00:02 +0900

    fix dir & file name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name  = 'slack-webhook',
-    version = '1.0.1',
+    version = '1.0.2',
     description = 'slack-webhook is a python client library for slack api Incoming Webhooks on Python 3.5 and above.',
     long_description = long_description,
     long_description_content_type="text/markdown",
