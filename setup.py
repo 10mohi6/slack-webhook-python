da2c74083171555cde27c9245f141c40b2238bea
10mohi6 <10.mohi.6.y@gmail.com>
2021-12-15 14:48:48 +0000

    update lib

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Intended Audience :: Developers",
         "Topic :: Communications :: Chat",
         "Topic :: System :: Networking",
